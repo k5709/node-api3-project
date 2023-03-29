810cd6ff782a759de445accda021e85ccb43e165
Kristian Rosales <r.kristian40@gmail.com>
2023-03-29 14:33:10 -0500

    wired project up

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,47 @@
+const User = require("../users/users-model");
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  // DO YOUR
+  const timestamp = new Date().toLocaleDateString;
+  const method = req.method;
+  const url = req.originalUrl;
+  console.log(`[${timestamp}] ${method} to ${url}`);
+  next();
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+async function validateUserId(req, res, next) {
+  try {
+    const user = await User.getById(req.params.id);
+    if (!user) {
+      res.status(404).json({ message: "no such user" });
+    } else {
+      req.user = user;
+      next();
+    }
+  } catch (error) {
+    //later
+    res.status(500).json({
+      message: "problem finding user",
+    });
+  }
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  // DO YOUR
+  console.log("test2");
+  next();
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  // DO YOUR
+  console.log("test");
+  next();
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+};
