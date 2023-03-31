81fe40ccd85d0ce02baf2a26ce1054c31e327032
Kristian Rosales <r.kristian40@gmail.com>
2023-03-31 12:55:41 -0500

    dumb error, met mvp

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -37,7 +37,7 @@ router.post("/", validateUser, (req, res, next) => {
 });
 
 router.put("/:id", validateUserId, validateUser, (req, res, next) => {
-  User.update(req.params.id, { name: req.user })
+  User.update(req.params.id, { name: req.name })
     .then(() => {
       return User.getById(req.params.id);
     })
